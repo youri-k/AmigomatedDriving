502969d3059d5850e6577e4e1204df5340d3d98c
Tobias <pito97.online@gmail.com>
2017-03-15 16:15:01 +0100

    Abstandskontrolle

--- src/platooning/scripts/follow_planner.py
+++ src/platooning/scripts/follow_planner.py
@@ -6,12 +6,25 @@ from nav_msgs.msg import Odometry
 
 
 
-command_velocity_publisher = None
+command_velocity_publisher = None #Publisher
+velocity = None #Twist
 
 
 def callback(data):
-    command_velocity_publisher.publish(data.twist.twist)
+    velocity = data.twist.twist
+    setMovement(velocity)    
+
+def abstandsKontrolle(abstand):
+    soll_Abstand = 0.1
+    if abstand > soll_Abstand:
+        velocity.linear.x *= 1.1
+    if abstand < soll_Abstand:
+        velocity.linear.x *= 0.9
     
+    setMovement(velocity)
+
+def setMovement(velocity):
+    command_velocity_publisher.publish(velocity)
 
 if __name__ == '__main__':
 
@@ -19,4 +32,13 @@ if __name__ == '__main__':
     rospy.Subscriber("/Achim/RosAria/pose", Odometry, callback)
     command_velocity_publisher = rospy.Publisher('RosAria/cmd_vel', Twist, queue_size=10)
 
+    x = 2
+    while x > -2:
+        abstandsKontrolle(x)
+        x -= 0.3
+        sleep(400)
+
     rospy.spin()
+
+
+
